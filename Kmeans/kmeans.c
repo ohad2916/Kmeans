61c2e947ca4742bdcccc1f31af6466fb637f2101
ohad2916 <ohadagadi50@gmail.com>
2022-12-17 19:31:31 +0200

    matched logic with actual algorithm.

--- Kmeans/kmeans.c
+++ Kmeans/kmeans.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include <string.h>
 
 #define DEFAULT_ITER 200
 #define Convergence_VALUE 0.001
@@ -41,14 +41,14 @@ int main(int argc, char** argv) {
 	double** curr_X;
 	size_t min_cluster_index;
 	double min_value, curr_euc_d;
-	double** curr_clusters;
+	double** new_cluster;
 	double* min_cluster;
-	double cluster_size;
 	double curr_Muk;
 	double max_Duk;
+	int converged;
 	N = 1;
 	d = 1;
-	data = NULL; p = NULL; b = NULL; c = NULL; cluster_mean = NULL; curr_clusters = NULL;
+	data = NULL; p = NULL; b = NULL; c = NULL; cluster_mean = NULL; new_cluster = NULL;
 
 	/*input validation*/
 	if (argc < 2 || argc > 3) {
@@ -72,7 +72,7 @@ int main(int argc, char** argv) {
 	input_data = calloc(capacity + 1,sizeof(char));
 	if (!input_data){
 		printf("An Error Has Occurred\n");
-		free_memory(b, c, p, curr_clusters, cluster_mean, data);
+		free_memory(b, c, p, new_cluster, cluster_mean, data);
 		return 1;
 	}
 	while ((ch = getchar()) != EOF) {
@@ -83,7 +83,7 @@ int main(int argc, char** argv) {
 			temp_input_data = realloc(input_data, sizeof(char) * capacity + 1);
 			if (!temp_input_data) {
 				printf("An Error Has Occurred\n");
-				free_memory(b, c, p, curr_clusters, cluster_mean, data);
+				free_memory(b, c, p, new_cluster, cluster_mean, data);
 				return 1;
 			}
 			else
@@ -110,14 +110,14 @@ int main(int argc, char** argv) {
 	data = calloc( N,sizeof(double*));
 	if (!data) {
 		printf("An Error Has Occurred\n");
-		free_memory(b, c, p, curr_clusters, cluster_mean, data);
+		free_memory(b, c, p, new_cluster, cluster_mean, data);
 		return 1;
 	}
 	p = NULL;
 	p = (double*)calloc(N * d, sizeof(double));
 	if (!p){
 		printf("An Error Has Occurred\n");
-		free_memory(b, c, p, curr_clusters, cluster_mean, data);
+		free_memory(b, c, p, new_cluster, cluster_mean, data);
 		return 1;
 	}
 	for (i = 0; i < N; i++) {
@@ -143,15 +143,15 @@ int main(int argc, char** argv) {
 	}*/
 	/*end convert
 	initialize clusters*/
-	b = calloc(K * (d + 1), sizeof(double));
+	b = calloc(K * d, sizeof(double));
 	cluster_mean = calloc(K , sizeof(double*));
 	if (!b || !cluster_mean){
 		printf("An Error Has Occurred\n");
-		free_memory(b, c, p, curr_clusters, cluster_mean, data);
+		free_memory(b, c, p, new_cluster, cluster_mean, data);
 		return 1;
 	}
 	for (i = 0; i < K; i++) {
-		cluster_mean[i] = b + i * (d + 1);
+		cluster_mean[i] = b + i * (d);
 	}
 
 	for (i = 0; i < K; i++)
@@ -166,70 +166,74 @@ int main(int argc, char** argv) {
 	i = 0;
 	/*allocate temporary clusters to decide convergence*/
 	c = calloc(K * (d + 1), sizeof(double));
-	curr_clusters = calloc(K , sizeof(double*));
-	if (!c || !curr_clusters) {
+	new_cluster = calloc(K , sizeof(double*));
+	if (!c || !new_cluster) {
 		printf("An Error Has Occurred\n");
-		free_memory(b, c, p, curr_clusters, cluster_mean, data);
+		free_memory(b, c, p, new_cluster, cluster_mean, data);
 		return 1;
 	}
 	for (i = 0; i < K; i++) {
-		curr_clusters[i] = c + i * (d + 1);
+		new_cluster[i] = c + i * (d + 1);
 	}
 
 	/*printf("#datapoints recieved: %lu of dimension:%lu\n", (unsigned long)N, (unsigned long)d);
 	printf("Iterating %lu times over %lu clusters\n", (unsigned long)iter, (unsigned long)K);*/
 	
 	i = 0;
-	while (i < iter)
+	converged = 0;
+	while (i < iter && !converged)
 	{	
-		/*copy current clusters to decide against.*/
-		for (j = 0; j < K; j++)
-		{
-			for (m = 0; m < d + 1; m++)
-			{
-				curr_clusters[j][m] = cluster_mean[j][m];
-			}
-		}
-		/*decide closest cluster against the copy and update it with new Xi*/
+		/*zero out new cluster array*/
+		memset(c, 0.0, K* (d + 1) * sizeof(double));
+		/*decide closest cluster against the original and update it with new Xi*/
 		for (m = 0; m < N ; m++) {
 			min_cluster_index = 0;
 			min_value = INT_MAX;
 			for (j = 0; j < K; j++)
 			{
-				curr_euc_d = euc_d(curr_clusters[j], *(curr_X + m), d);
+				curr_euc_d = euc_d(cluster_mean[j], *(curr_X + m), d);
 				if (curr_euc_d < min_value)
 				{
 					min_value = curr_euc_d;
 					min_cluster_index = j;
 				}
 			}
-			/*updating real one*/
-			min_cluster = cluster_mean[min_cluster_index];
-			cluster_size = min_cluster[d];
+			/*updating new cluster, just adding for now. divide later.*/
+			min_cluster = new_cluster[min_cluster_index];
 			for (j = 0; j < d; j++)
 			{
-				/*min_cluster[js] *= (cluster_size);
-				min_cluster[j] += (curr_X[i][j]);
-				min_cluster[j] /= (cluster_size + 1);*/
-				min_cluster[j] = ((min_cluster[j] / (cluster_size + 1)) * cluster_size) + (curr_X[m][j] / (cluster_size + 1));
+				min_cluster[j] += curr_X[m][j];
 			}
 			min_cluster[d]++;
+		}
+		/*calculate the actual means*/
+		for (j = 0; j < K; j++){
+			for (m = 0; m < d; m++){
+				new_cluster[j][m] /= new_cluster[j][d];
+			}
 		}
 
 		/*decide convegerence*/
 		max_Duk = 0;
 		for (j = 0; j < K; j++) {
-			curr_Muk = euc_d(cluster_mean[j], curr_clusters[j], d);
+			curr_Muk = euc_d(cluster_mean[j], new_cluster[j], d);
 			if (curr_Muk > max_Duk)
 				max_Duk = curr_Muk;
 		}
 		if (max_Duk <= Convergence_VALUE) {
 			/*print statement for debugging*/
 			/*printf("Converged after: %d iterations\n", (int)i + 1);*/
-			break;
+			converged = 1;
 		}
-
 		i++;
+		/*copy new cluster to old ones*/
+		for (j = 0; j < K; j++)
+		{
+			for (m = 0; m < d; m++)
+			{
+				cluster_mean[j][m] = new_cluster[j][m];
+			}
+		}
 	}
 	for (m = 0; m < K; m++)
 	{
@@ -241,11 +245,11 @@ int main(int argc, char** argv) {
 		}
 		printf("\n");
 	}
-	free_memory(b, c, p, curr_clusters, cluster_mean, data);
+	free_memory(b, c, p, new_cluster, cluster_mean, data);
 	/*free(b);
 	free(c);
 	free(p);
-	free(curr_clusters);
+	free(new_cluster);
 	free(cluster_mean);
 	free(data);*/
 
